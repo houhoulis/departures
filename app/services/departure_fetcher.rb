0c93d699dedbb5513ed6219fee735d75a4bc8a8c
Chris Houhoulis <chris@chrishouhoulis.com>
2018-04-02 08:33:03 -0400

    Service object to retrieve departure statuses.

---
+++ app/services/departure_fetcher.rb
@@ -0,0 +1,41 @@
+require 'csv'
+
+class DepartureFetcher
+  attr_reader :data
+
+  URL = URI("http://developer.mbta.com/lib/gtrtfs/Departures.csv")
+
+  def self.call
+    new.call
+  end
+
+  def call
+    Rails.logger.info "#{self.class} destroying #{Departure.count} Departures."
+    Departure.destroy_all
+
+    @data = StringIO.new(import_feed URL)
+
+    create_departures
+  end
+
+  def import_feed url = URL
+    Net::HTTP.get url
+  end
+
+  def create_departures
+    departures = CSV.parse data.read
+    header_row = departures.shift
+
+    Rails.logger.info "#{self.class} creating #{departures.count} Departures at #{Time.now}."
+    departures.map do |time_stamp, origin, trip, destination, scheduled_time, lateness, track, status|
+      Departure.create! time_stamp: Time.at(time_stamp.to_i),
+                        origin: origin,
+                        trip: trip,
+                        destination: destination,
+                        scheduled_time: Time.at(scheduled_time.to_i),
+                        lateness: lateness,
+                        track: track,
+                        status: status
+    end
+  end
+end
