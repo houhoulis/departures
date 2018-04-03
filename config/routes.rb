c956db97642f8f7868d62b500bcf91d5bc19ec6c
Chris Houhoulis <chris@chrishouhoulis.com>
2018-04-03 00:28:57 -0400

    Route root page to 'boards#index'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'boards#index'
   resources :boards
   # resources :departures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
