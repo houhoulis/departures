e5c887abeb4258757d956952c27a542aed34e611
Chris Houhoulis <chris@chrishouhoulis.com>
2018-04-03 11:46:11 -0400

    Configure app to run in Massachusetts's time zone.

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module Departures
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = "America/New_York"
   end
 end
